dc62db63d470f07b93b3e08c0037df1e61119e5e
Marco Arruda <marco.nc.arruda@gmail.com>
2022-03-01 18:00:07 -0300

    Adjust unit tests

--- tests/unit/Timeline.spec.ts
+++ tests/unit/Timeline.spec.ts
@@ -1,19 +1,44 @@
 import { nextTick } from "vue";
-import { mount } from "@vue/test-utils";
+import { flushPromises, mount } from "@vue/test-utils";
 
 import Timeline from "../../src/components/Timeline.vue";
 import { today, thisWeek, thisMonth } from "../../src/mocks";
 
+jest.mock('axios', () => ({
+  get: (url: string) => {
+    return Promise.resolve({
+      data: [today, thisWeek, thisMonth]
+    })
+  }
+}))
+
+function mountTimeline() {
+  return mount({
+    components: { Timeline },
+    template: `
+    <suspense>
+      <template #default>
+        <timeline />
+      </template>
+      <template #fallback>
+        Loading...
+      </template>
+    </suspense>`
+  });
+}
+
 describe("Timeline", () => {
-  it("renders today post by default", () => {
-    const wrapper = mount(Timeline);
+  it("renders today post by default", async () => {
+    const wrapper = mountTimeline()
+    await flushPromises()
 
     expect(wrapper.html()).toContain(today.created.format("Do MMM"));
   });
 
   // requestAnimationFrame(() => ...)
   it("update when the period is click", async () => {
-    const wrapper = mount(Timeline);
+    const wrapper = mountTimeline()
+    await flushPromises()
 
     // click and wait for the next frame
     await wrapper.get('[data-test="This Week"]').trigger("click");
@@ -25,7 +50,8 @@ describe("Timeline", () => {
 
   // requestAnimationFrame(() => ...)
   it("update when the period is click", async () => {
-    const wrapper = mount(Timeline);
+    const wrapper = mountTimeline()
+    await flushPromises()
 
     // click and wait for the next frame
     await wrapper.get('[data-test="This Month"]').trigger("click");
