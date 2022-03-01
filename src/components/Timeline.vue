79fab69a297718085053f8b650a8569c3e62e037
Marco Arruda <marco.nc.arruda@gmail.com>
2022-02-28 23:03:28 -0300

    changing cmp name

---
+++ src/components/Timeline.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="is-primary panel">
+    <span class="panel-tabs">
+      <a>Today</a>
+      <a>This week</a>
+      <a>This month</a>
+    </span>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Timeline',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
