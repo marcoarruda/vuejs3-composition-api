48ee2237dbb9fbdd95f0dc77276cfdd6006f9070
Marco Arruda <marco.nc.arruda@gmail.com>
2022-03-01 17:22:25 -0300

    create store for posts

---
+++ src/store.ts
@@ -0,0 +1,36 @@
+import { reactive, readonly } from "vue";
+import { Post } from "../src/mocks";
+
+interface State {
+  posts: PostsState;
+}
+
+interface PostsState {
+  ids: string[]; // [1, 2, 3, 4]
+
+  all: Map<string, Post>;
+
+  loaded: boolean;
+}
+
+class Store {
+  private state: State;
+
+  constructor(initial: State) {
+    this.state = reactive(initial);
+  }
+
+  getState() {
+    return readonly(this.state);
+  }
+}
+
+const store = new Store({
+  posts: {
+    all: new Map(),
+    ids: [],
+    loaded: false,
+  },
+});
+
+store.getState().posts.loaded
