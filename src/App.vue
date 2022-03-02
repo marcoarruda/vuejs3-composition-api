a1a260e53766b8e873e57047c88885c9c4606aca
Marco Arruda <marco.nc.arruda@gmail.com>
2022-03-02 18:09:16 -0300

    create new post page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="section">
     <div class="container">
+      <navbar />
       <router-view />
     </div>
   </section>
@@ -9,8 +10,11 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 
+import Navbar from "./components/Navbar.vue";
+
 export default defineComponent({
   name: "App",
+  components: { Navbar },
 });
 </script>
 
