304b32cd7cc441d531321ddf042c61065033a09e
Marco Arruda <marco.nc.arruda@gmail.com>
2022-03-02 18:01:13 -0300

    create router, home component and refactor app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <template>
   <section class="section">
     <div class="container">
-      <suspense>
-        <template #default>
-          <timeline />
-        </template>
-        <template #fallback>
-          <spinner />
-        </template>
-      </suspense>
+      <router-view />
     </div>
   </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Spinner from './components/Spinner.vue'
-import Timeline from "./components/Timeline.vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    Spinner,
-    Timeline,
-  },
 });
 </script>
 
